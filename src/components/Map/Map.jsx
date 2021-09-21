823dffb2ab4146d0fb0e48ec7ce1cc8937d8a5dd
Irena Chen <irena1009@gmail.com>
2021-09-21 15:43:26 -0700

    add map

--- src/components/Map/Map.jsx
+++ src/components/Map/Map.jsx
@@ -1,15 +1,33 @@
 import React from 'react'
 import GoogleMapReact from 'google-map-react'
 import { Paper, Typography, useMediaQuery} from '@material-ui/core' //Paper: a div with background color, useMediaQuery: make the map more reponsive on mobile
-import LocationOutlineIcon from '@material-ui/icons/LocationOutlined'
+import LocationOnOutlineIcon from '@material-ui/icons/LocationOnOutlined'
 import Rating from '@material-ui/lab' //importing from lab because it's still been working on
 
 import useStyles from './styles'
 
 const Map = () => {
+    const classes = useStyles()
+    const isMobile = useMediaQuery('(min-width: 600px)') //isMobile will be set to be false if the media is larger than 600px
+
+    const coordinates = {lat: 0, lng: 0}
+
     return (
-        <div>
-            Map
+        <div className={classes.mapContainer}>
+            <GoogleMapReact 
+                bootstrapURLKeys={{ key: '' }}
+               
+                defaultCenter={ coordinates }
+                center={ coordinates }
+                defaultZoom={14}
+                margin={[50, 50, 50, 50]}
+                options={''}
+                onChange={''}
+                onChildClick={''}
+            >
+                {/*  create new projects on google developers' console  */}
+                
+            </GoogleMapReact> 
         </div>
     )
 }

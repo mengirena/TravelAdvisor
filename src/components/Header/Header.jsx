df414b78cfcc758bbfae7c0e522d025f054ba14a
Irena Chen <irena1009@gmail.com>
2021-09-21 15:21:45 -0700

    add map style

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -3,7 +3,11 @@ import { Autocomplete } from '@react-google-maps/api'
 import { AppBar, Toolbar, Typography, InputBase, Box } from '@material-ui/core' //Typography: every single text element but can change the variant, Box: it's basically a div can simply set display property and others
 import SearchIcon from '@material-ui/icons/Search'
 
+import useStyles from './styles'
+
 const Header = () => {
+    const classes = useStyles()
+
     return (
         <AppBar position='static'>
             <Toolbar className={classes.toolbar}>
@@ -14,14 +18,14 @@ const Header = () => {
                     <Typography variant="h6" className={classes.title}>
                         Explore new places
                     </Typography>
-                    <Autocomplete>
+                    {/* <Autocomplete> */}
                         <div className={classes.search}>
                             <div className={classes.searchIcon}>
                                 <SearchIcon />
                             </div>
                             <InputBase placeholder="Search..." classes={{ root: classes.inputRoot, input: classes.inputInput}} />
                         </div>
-                    </Autocomplete>
+                    {/* </Autocomplete> */}
                 </Box>
             </Toolbar>
         </AppBar>
--- src/components/Map/Map.jsx
+++ src/components/Map/Map.jsx
@@ -1,4 +1,10 @@
 import React from 'react'
+import GoogleMapReact from 'google-map-react'
+import { Paper, Typography, useMediaQuery} from '@material-ui/core' //Paper: a div with background color, useMediaQuery: make the map more reponsive on mobile
+import LocationOutlineIcon from '@material-ui/icons/LocationOutlined'
+import Rating from '@material-ui/lab' //importing from lab because it's still been working on
+
+import useStyles from './styles'
 
 const Map = () => {
     return (

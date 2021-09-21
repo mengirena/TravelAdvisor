cff1b57495b002a344f0799aebad90e969684c34
Irena Chen <irena1009@gmail.com>
2021-09-21 14:32:34 -0700

    add styles

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,10 +1,30 @@
 import React from 'react'
+import { Autocomplete } from '@react-google-maps/api'
+import { AppBar, Toolbar, Typography, InputBase, Box } from '@material-ui/core' //Typography: every single text element but can change the variant, Box: it's basically a div can simply set display property and others
+import SearchIcon from '@material-ui/icons/Search'
 
 const Header = () => {
     return (
-        <div>
-            <h1>Header</h1>
-        </div>
+        <AppBar position='static'>
+            <Toolbar className={classes.toolbar}>
+                <Typography variant="h5" className={classes.title}>
+                    Travel Advisor
+                </Typography>
+                <Box display="flex">
+                    <Typography variant="h6" className={classes.title}>
+                        Explore new places
+                    </Typography>
+                    <Autocomplete>
+                        <div className={classes.search}>
+                            <div className={classes.searchIcon}>
+                                <SearchIcon />
+                            </div>
+                            <InputBase placeholder="Search..." classes={{ root: classes.inputRoot, input: classes.inputInput}} />
+                        </div>
+                    </Autocomplete>
+                </Box>
+            </Toolbar>
+        </AppBar>
     )
 }
 

b125399a0f0b1a438932d78ea295d1d51cea9b28
Irena Chen <irena1009@gmail.com>
2021-10-10 21:48:35 +0800

    create List

--- src/components/List/List.jsx
+++ src/components/List/List.jsx
@@ -1,9 +1,33 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { CircularProgress, Grid, Typography, InputLabel, MenuItem, FormControl, Select } from '@material-ui/core'
+
+import useStyles from './styles'
 
 const List = () => {
+
+    const classes = useStyles()
+    const [type, setType] = useState('restaurants')
+    const [rating, setRating] = useState('')
+
     return (
-        <div>
-            list
+        <div className={classes.container}>
+            <Typography variant="h4">Restaurants, Hotels & Attractions around you</Typography>
+            <FormControl className={classes.formControl}>
+                <InputLabel>Type</InputLabel>
+                <Select value={type} onChange={(e) => {e.target.value}}>
+                    <MenuItem value="restaurants">Restaurants</MenuItem>
+                    <MenuItem value="hotels">Hotels</MenuItem>
+                    <MenuItem value="attractions">Attractions</MenuItem>
+                </Select>
+            </FormControl>
+            <FormControl className={classes.formControl}>
+                <InputLabel>Rating</InputLabel>
+                <Select value={rating} onChange={(e) => {e.target.value}}>
+                    <MenuItem value="restaurants">Restaurants</MenuItem>
+                    <MenuItem value="hotels">Hotels</MenuItem>
+                    <MenuItem value="attractions">Attractions</MenuItem>
+                </Select>
+            </FormControl>
         </div>
     )
 }

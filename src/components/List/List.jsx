2286823e512da2b566f990ef452d9ffc8cb53249
Irena Chen <irena1009@gmail.com>
2021-10-11 23:08:49 +0800

    List

--- src/components/List/List.jsx
+++ src/components/List/List.jsx
@@ -14,7 +14,7 @@ const List = () => {
             <Typography variant="h4">Restaurants, Hotels & Attractions around you</Typography>
             <FormControl className={classes.formControl}>
                 <InputLabel>Type</InputLabel>
-                <Select value={type} onChange={(e) => {e.target.value}}>
+                <Select value={type} onChange={(e) => setType(e.target.value)}>
                     <MenuItem value="restaurants">Restaurants</MenuItem>
                     <MenuItem value="hotels">Hotels</MenuItem>
                     <MenuItem value="attractions">Attractions</MenuItem>
@@ -22,10 +22,11 @@ const List = () => {
             </FormControl>
             <FormControl className={classes.formControl}>
                 <InputLabel>Rating</InputLabel>
-                <Select value={rating} onChange={(e) => {e.target.value}}>
-                    <MenuItem value="restaurants">Restaurants</MenuItem>
-                    <MenuItem value="hotels">Hotels</MenuItem>
-                    <MenuItem value="attractions">Attractions</MenuItem>
+                <Select value={rating} onChange={(e) => setRating(e.target.value)}>
+                    <MenuItem value={0}>All</MenuItem>
+                    <MenuItem value={3}>Above 3.0</MenuItem>
+                    <MenuItem value={4}>Avobe 4.0</MenuItem>
+                    <MenuItem value={4.5}>Avobe 4.5</MenuItem>
                 </Select>
             </FormControl>
         </div>
